043bbde564867e353f6c2a3782cdbd6eace89371
Gemechis27 <game.daba19@gmail.com>
2025-06-07 00:25:00 +0300

    get answer by question Id

--- app.js
+++ app.js
@@ -1,4 +1,31 @@
-<h1>
-    hellow world
-    
-</h1>
+
+const mysql2=require('mysql2')
+
+
+const dbconnection=mysql2.createpool({
+    user:"Evangadi-admin",
+    database:"evangadi-db",
+    host:"localhost",
+    password:"Evangadi123456$$",
+    connectionLimit:10
+})
+
+// async function start(){
+//     try {
+//         const result=await dbconnection.excute("select 'test'")
+//     } catch (error) {
+//         console.log(error.message);
+        
+//     }
+// }
+dbconnection.execute ("select 'test' ", (err,result)=>{
+    if(err){
+        console.log(err.message);
+        
+    } else{
+        console.log(result);
+        
+    }
+})
+
+
