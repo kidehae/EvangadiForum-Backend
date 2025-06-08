b468657feb608db3f659ee82a781f2a3415d6b3f
seblewongel-kassa <titigswk@gmail.com>
2025-06-08 01:49:13 -1000

    updated table

--- Table/schema.js
+++ Table/schema.js
@@ -15,15 +15,17 @@ export let questions = `CREATE TABLE IF NOT EXISTS questions(
     title VARCHAR(50) NOT NULL,
     description VARCHAR(200) NOT NULL,
     tag VARCHAR(20),
+    createdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (id, questionid),
     FOREIGN KEY(userid) REFERENCES users(userid)
 )`;
 
-export let answers =`CREATE TABLE IF NOT EXISTS answers(
+export let answers = `CREATE TABLE IF NOT EXISTS answers(
     answerid INT(20) NOT NULL,
     userid INT(20) NOT NULL,
     questionid VARCHAR(100) NOT NULL,
     answer VARCHAR(200) NOT NULL,
+    createdate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (answerid),
     FOREIGN KEY(userid) REFERENCES users(userid)
 )`;
