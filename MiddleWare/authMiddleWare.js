7b7328c26529fd47bfa1f8d8b0fd04a94f9b0db0
kidehae <149243635+kidehae@users.noreply.github.com>
2025-06-16 17:53:59 +0300

    Update authMiddleWare.js

--- MiddleWare/authMiddleWare.js
+++ MiddleWare/authMiddleWare.js
@@ -1,34 +1,32 @@
-const {StatusCodes}=require("http-status-codes")
-const jwt=require("jsonwebtoken")
+const { StatusCodes } = require("http-status-codes");
+const jwt = require("jsonwebtoken");
 
+async function authMiddleware(req, res, next) {
+  const authHeader = req.headers.authorization;
 
-async function authMiddleware(req,res,next) {
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    return res
+      .status(StatusCodes.UNAUTHORIZED)
+      .json({ msg: "Authentication invalid" });
+  }
+  //  removing bearer prefix from the authHeader
 
-const authHeader=req.headers.authorization
+  const token = authHeader.split(" ")[1];
+  const JWT_SECRET = "wjiLHtT2OgGnVlfZZzywl0cb9DzvWRHx";
 
-if (!authHeader || !authHeader.startsWith('Bearer')){
-    
-    return res.status(StatusCodes.UNAUTHORIZED).json({msg:'Authentication invalid'})
-}
-//  removing bearer prefix from the authHeader
+  try {
+    const data = jwt.verify(token, JWT_SECRET);
+    req.user = { username: data.username, userid: data.userid };
 
-const token = authHeader.slice(7);
-
-try {
-const data= jwt.verify(token,process.env.JWT_SECRET)
-req.user = { username: data.username, userid: data.userid };
-
-next()
-
-}catch (error) {
-const message =
-    error.name === "TokenExpiredError"
+    next();
+  } catch (error) {
+    const message =
+      error.name === "TokenExpiredError"
         ? "Token expired"
         : "Authentication invalid";
 
-return res.status (StatusCodes.UNAUTHORIZED).json({msg:message})
-}
+    return res.status(StatusCodes.UNAUTHORIZED).json({ msg: message });
+  }
 }
 
-
-module.exports=authMiddleware
+module.exports = authMiddleware;
