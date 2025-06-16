e491e6aaa0ed032a9211762c60abc65adff029fc
kidehae <149243635+kidehae@users.noreply.github.com>
2025-06-16 17:53:29 +0300

    Update dbConfig.js

--- Db/dbConfig.js
+++ Db/dbConfig.js
@@ -2,11 +2,16 @@ const mysql2 = require("mysql2");
 
 const dbConnection = mysql2.createPool({
   // socketPath: process.env.DB_SOCKET_PATH,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // host: process.env.DB_HOST,
+  // database: process.env.DB_NAME,
+  // connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
+  user: "evangadi-meaza",
+  password: "Evangadi123456$$",
+  host: "localhost",
+  database: "evangadi-db",
+  connectionLimit: 10,
 });
 
 module.exports = dbConnection.promise();
