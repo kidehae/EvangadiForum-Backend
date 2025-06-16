2bcc6684a1833aeebe291fed7b1dfec24c5bf446
kidehae <149243635+kidehae@users.noreply.github.com>
2025-06-16 18:29:14 +0300

    Update dbConfig.js

--- Db/dbConfig.js
+++ Db/dbConfig.js
@@ -7,11 +7,16 @@ const dbConnection = mysql2.createPool({
   // host: process.env.DB_HOST,
   // database: process.env.DB_NAME,
   // connectionLimit: parseInt(process.env.DB_CONNECTION_LIMIT) || 10,
-  user: "evangadi-meaza",
-  password: "Evangadi123456$$",
-  host: "localhost",
-  database: "evangadi-db",
+  userUsername: "forumbackend_user",
+  password: "iqJ5dz7FFCbu5ec4by0Yp5QI45dTfCch",
+  host: "dpg-d183d9p5pdvs73bq6hb0-a",
+  database: "forumbackend",
+  Internal Database URL: "postgresql://forumbackend_user:iqJ5dz7FFCbu5ec4by0Yp5QI45dTfCch@dpg-d183d9p5pdvs73bq6hb0-a/forumbackend",
+  External Database URL: "postgresql://forumbackend_user:iqJ5dz7FFCbu5ec4by0Yp5QI45dTfCch@dpg-d183d9p5pdvs73bq6hb0-a.oregon-postgres.render.com/forumbackend",
+  PSQL Command: "PGPASSWORD=iqJ5dz7FFCbu5ec4by0Yp5QI45dTfCch psql -h dpg-d183d9p5pdvs73bq6hb0-a.oregon-postgres.render.com -U forumbackend_user forumbackend",
   connectionLimit: 10,
+  Port: 5432
+  
 });
 
 module.exports = dbConnection.promise();
